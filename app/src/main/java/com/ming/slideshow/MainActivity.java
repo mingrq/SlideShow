7cf644551d079421c0defe7c1d217919222ef74b
002 <mingruqi@sina.cn>
2018-12-18 14:45:57 +0800

    补充

--- app/src/main/java/com/ming/slideshow/MainActivity.java
+++ app/src/main/java/com/ming/slideshow/MainActivity.java
@@ -2,6 +2,7 @@ package com.ming.slideshow;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -19,18 +20,29 @@ public class MainActivity extends AppCompatActivity {
 
         List<Info> mList = new ArrayList<>();
         Info info = new Info();
+        info.setBannerTitle("标题0");
         info.setBannerUri("https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=2097124721,3074829049&fm=27&gp=0.jpg");
         Info info1 = new Info();
+        info1.setBannerTitle("标题1");
         info1.setBannerUri("https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=3034670085,1677733933&fm=27&gp=0.jpg");
         Info info2 = new Info();
+        info2.setBannerTitle("标题2");
         info2.setBannerUri("http://imgsrc.baidu.com/forum/pic/item/b64543a98226cffc8872e00cb9014a90f603ea30.jpg");
         Info info3 = new Info();
+        info3.setBannerTitle("标题3");
         info3.setBannerUri("http://imgsrc.baidu.com/forum/pic/item/261bee0a19d8bc3e6db92913828ba61eaad345d4.jpg");
         mList.add(info);
         mList.add(info1);
         mList.add(info2);
         mList.add(info3);
+        mList.add(info1);
         slideShow.setData(mList);
+        slideShow.setSlideOnItemClickLisenter(new SlideShow.OnItemClickLisenter() {
+            @Override
+            public void onClickLisenter(int position) {
+                Toast.makeText(MainActivity.this,String.valueOf(position),Toast.LENGTH_LONG).show();
+            }
+        });
         slideShow.commit();
     }
 }
--- slideshow_lib/src/main/java/ming/com/slideshow_lib/SlideShow.java
+++ slideshow_lib/src/main/java/ming/com/slideshow_lib/SlideShow.java
@@ -1,19 +1,19 @@
 package ming.com.slideshow_lib;
 
-import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.res.TypedArray;
 import android.graphics.drawable.Drawable;
 import android.graphics.drawable.StateListDrawable;
+import android.os.Handler;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.util.AttributeSet;
+import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.BaseAdapter;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
@@ -33,7 +33,7 @@ import java.util.List;
 public class SlideShow extends FrameLayout {
 
     private Context context;
-    private RadioGroup radioGroup;
+    private LinearLayout indicator;
     private TextView title;
     private ViewPager banners;
 
@@ -53,7 +53,6 @@ public class SlideShow extends FrameLayout {
      * 指示器大小
      */
     private float indicatorSize;
-
 
     /**
      * 标题文字颜色
@@ -78,8 +77,22 @@ public class SlideShow extends FrameLayout {
      */
     private boolean isTitle = true;
 
+
+    /**
+     * 指示器间距
+     */
+    private float indicatorMargin;
+
+    /**
+     * 滚动时间
+     */
+    private int slideTime;
+    private int indicatorEn;
     private TypedArray array;
     private List<ImageView> bannerlist;
+    private boolean isAutoPlay = true;
+    private Runnable task;
+    private OnItemClickLisenter onItemClickLisenter;
 
     public SlideShow(@NonNull Context context) {
         this(context, null);
@@ -93,7 +106,7 @@ public class SlideShow extends FrameLayout {
         super(context, attrs, defStyleAttr);
         this.context = context;
         LayoutInflater.from(context).inflate(R.layout.slideshow, this);
-        radioGroup = findViewById(R.id.indicator);
+        indicator = findViewById(R.id.indicator);
         title = findViewById(R.id.banner_title);
         banners = findViewById(R.id.vp_banner);
         array = context.obtainStyledAttributes(attrs, R.styleable.SlideShow);
@@ -103,11 +116,27 @@ public class SlideShow extends FrameLayout {
     private void init() {
         setIndicatorSelecter(array.getResourceId(R.styleable.SlideShow_indicatorSelecter, R.drawable.selecter_slideindicator));
         setIndicatorBgColor(array.getColor(R.styleable.SlideShow_indicatorBgColor, 0x2A000000));
-        setIndicatorSize(array.getDimension(R.styleable.SlideShow_indicatorSize, 10));
-        setIndicatorHeight(array.getDimension(R.styleable.SlideShow_indicatorHeight, 40));
+        setIndicatorSize(array.getDimension(R.styleable.SlideShow_indicatorSize, dp2px(6)));
+        setIndicatorHeight(array.getDimension(R.styleable.SlideShow_indicatorHeight, dp2px(30)));
         setTitleColor(array.getColor(R.styleable.SlideShow_titleColor, 0xffffffff));
-        setTitleSize(array.getDimension(R.styleable.SlideShow_titleSize, 10));
+        setTitleSize(array.getDimension(R.styleable.SlideShow_titleSize, dp2px(10)));
+        setSlideTime(array.getInteger(R.styleable.SlideShow_slideTime, 4000));
+        setIndicatorMargin(array.getDimension(R.styleable.SlideShow_indicatorMargin, dp2px(6)));
         array.recycle();
+    }
+
+    /**
+     * 获取指示器间距
+     */
+    public float getIndicatorMargin() {
+        return indicatorMargin;
+    }
+
+    /**
+     * 设置指示器间距
+     */
+    public void setIndicatorMargin(float indicatorMargin) {
+        this.indicatorMargin = indicatorMargin;
     }
 
     /**
@@ -140,7 +169,7 @@ public class SlideShow extends FrameLayout {
      */
     public void setIndicatorBgColor(int indicatorBgColor) {
         this.indicatorBgColor = indicatorBgColor;
-        radioGroup.setBackgroundColor(indicatorBgColor);
+        indicator.setBackgroundColor(indicatorBgColor);
     }
 
     /**
@@ -156,7 +185,21 @@ public class SlideShow extends FrameLayout {
     public void setIndicatorHeight(float indicatorHeight) {
         this.indicatorHeight = indicatorHeight;
         LinearLayout.LayoutParams params = new RadioGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, (int) indicatorHeight);
-        radioGroup.setLayoutParams(params);
+        indicator.setLayoutParams(params);
+    }
+
+    /**
+     * 获取滚动时间
+     */
+    public int getSlideTime() {
+        return slideTime;
+    }
+
+    /**
+     * 设置滚动时间
+     */
+    public void setSlideTime(int slideTime) {
+        this.slideTime = slideTime;
     }
 
     /**
@@ -195,11 +238,6 @@ public class SlideShow extends FrameLayout {
     }
 
     /**
-     * =================================================================================================================
-     */
-
-
-    /**
      * 设置指示器
      *
      * @param indicator   当前显示状态
@@ -211,6 +249,7 @@ public class SlideShow extends FrameLayout {
         Drawable checked = context.getResources().getDrawable(indicator);
         drawable.addState(new int[]{android.R.attr.checkable}, checked);
         drawable.addState(new int[]{-android.R.attr.checkable}, normal);
+
     }
 
     /**
@@ -220,7 +259,6 @@ public class SlideShow extends FrameLayout {
      */
     public void setIndicatorSelecter(int indicatorSelecter) {
         mIndicatorSelecter = indicatorSelecter;
-        Drawable drawable = context.getResources().getDrawable(mIndicatorSelecter);
     }
 
     /**
@@ -235,12 +273,34 @@ public class SlideShow extends FrameLayout {
     /**
      * 设置点击监听
      */
-    public void setSlideOnItemClickLisenter() {
-
+    public void setSlideOnItemClickLisenter(OnItemClickLisenter onItemClickLisenter) {
+        this.onItemClickLisenter = onItemClickLisenter;
     }
 
     public void commit() {
         initBannerImage();
+        setIndicator();
+        bindingViewPager();
+        initSlide();
+        new Handler().postDelayed(task, slideTime);
+    }
+
+    /**
+     * =================================================================================================================
+     */
+    /**
+     * 开始轮播
+     */
+    private void initSlide() {
+        task = new Runnable() {
+            @Override
+            public void run() {
+                if (isAutoPlay) {
+                    banners.setCurrentItem(banners.getCurrentItem() + 1);
+                }
+                new Handler().postDelayed(task, slideTime);
+            }
+        };
     }
 
     /**
@@ -272,13 +332,79 @@ public class SlideShow extends FrameLayout {
         }
         BannerViewPager bannerViewPager = new BannerViewPager();
         banners.setAdapter(bannerViewPager);
+        banners.setCurrentItem(1);
     }
 
     /**
      * 设置指示器
      */
     private void setIndicator() {
+        for (int k = 0; k < infoList.size(); k++) {
+            View vIndicator = new View(context);
+            vIndicator.setId(k);
+            int size = (int) (indicatorSize + 0.5f);
+            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(size, size);
+            params.leftMargin = (int) (indicatorMargin / 2 + 0.5f);
+            params.rightMargin = (int) (indicatorMargin / 2 + 0.5f);
+            vIndicator.setLayoutParams(params);
+            vIndicator.setBackgroundResource(mIndicatorSelecter);
+            vIndicator.setEnabled(false);
+            indicator.addView(vIndicator);
+        }
+        indicatorEn = 0;
+        indicator.getChildAt(indicatorEn).setEnabled(true);
+    }
 
+    /**
+     * viewpager绑定指示器
+     */
+    private void bindingViewPager() {
+        banners.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int i, float v, int i1) {
+
+            }
+
+            @Override
+            public void onPageSelected(int i) {
+                int currentItem = -1;
+                if (i == 0) {
+                    currentItem = infoList.size() - 1;
+                } else if (i == infoList.size() + 1) {
+                    currentItem = 0;
+                } else {
+                    currentItem = i - 1;
+                }
+                indicator.getChildAt(indicatorEn).setEnabled(false);
+                indicator.getChildAt(currentItem).setEnabled(true);
+                indicatorEn = currentItem;
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int i) {
+                if (i == ViewPager.SCROLL_STATE_IDLE) {//当前item完全显示
+                    isAutoPlay = true;
+                    if (banners.getCurrentItem() == 0) {
+                        banners.setCurrentItem(infoList.size(), false);
+                    } else if (banners.getCurrentItem() == infoList.size() + 1) {
+                        banners.setCurrentItem(1, false);
+                    }
+                } else if (i == ViewPager.SCROLL_STATE_DRAGGING) {//正在滑动
+                    isAutoPlay = false;
+                } else if (i == ViewPager.SCROLL_STATE_SETTLING) {//滑动到最后一个
+                    isAutoPlay = true;
+                }
+
+            }
+        });
+    }
+
+    /**
+     * dp转px
+     */
+    private int dp2px(float dpValues) {
+        dpValues = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dpValues, context.getResources().getDisplayMetrics());
+        return (int) (dpValues + 0.5f);
     }
 
 
@@ -311,10 +437,32 @@ public class SlideShow extends FrameLayout {
 
         @NonNull
         @Override
-        public Object instantiateItem(@NonNull ViewGroup container, int position) {
-            View view = bannerlist.get(position);
+        public Object instantiateItem(@NonNull ViewGroup container, final int position) {
+            ImageView view = bannerlist.get(position);
+            view.setScaleType(ImageView.ScaleType.FIT_XY);
             container.addView(view);
+            view.setOnClickListener(new OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    int currentItem;
+                    if (position == 0) {
+                        currentItem = bannerlist.size() - 2;
+                    } else if (position == bannerlist.size() - 1) {
+                        currentItem = 0;
+                    } else {
+                        currentItem = position - 1;
+                    }
+                    onItemClickLisenter.onClickLisenter(currentItem);
+                }
+            });
             return view;
         }
+    }
+
+    /**
+     * 点击监听
+     */
+    public interface OnItemClickLisenter {
+        void onClickLisenter(int position);
     }
 }
